ee3140cedad3065d499617baf7b033c01fa014a2
devd254 <devond156@gmail.com>
2023-12-29 15:42:26 -0500

    base navigation bar

---
+++ discord_clone/components/navigation/navigation-sidebar.tsx
@@ -0,0 +1,42 @@
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { Separator } from "@/components/ui/separator"
+import NavigationAction from "@/components/navigation/navigation-action";
+
+  
+
+export const NavigationSidebar = async() => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirect("/");
+    }
+
+    const servers = await db.server.findMany({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    return (
+        <div 
+            className = "space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3"
+        >
+            <NavigationAction />
+            
+        </div>
+    )
+}
