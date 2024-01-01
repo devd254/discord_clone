8630d1136f20d8ee01d1eb91c53c7084ca1714fa
devd254 <devond156@gmail.com>
2024-01-01 17:19:57 -0500

    Server sidebar options

--- discord_clone/components/server/server-header.tsx
+++ discord_clone/components/server/server-header.tsx
@@ -3,12 +3,14 @@
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
 
-import { ChevronDown } from "lucide-react";
+import { ChevronDown, LogOut, Plus, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
 
 import { 
     DropdownMenu,
     DropdownMenuContent,
-    DropdownMenuTrigger
+    DropdownMenuTrigger,
+    DropdownMenuItem,
+    DropdownMenuSeparator
 } from "@/components/ui/dropdown-menu";
 
 interface ServerHeaderProps {
@@ -20,8 +22,9 @@ export const ServerHeader = ({
     server,
     role
 }: ServerHeaderProps) => {
-    const isAdmin = role ===MemberRole.ADMIN;
+    const isAdmin = role === MemberRole.ADMIN;
     const isModerator = isAdmin || role === MemberRole.MODERATOR;
+    const isMember = role === MemberRole.GUEST;
 
     return (
         <DropdownMenu>
@@ -30,7 +33,7 @@ export const ServerHeader = ({
               asChild
             >
                 <button
-                  className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover: bg-zing-700/10 dark:hover:bg-zinc-700/50 transition"                  
+                  className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover: bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"                  
                 >
                     {server.name}
                     <ChevronDown className="h-5 w-5 ml-auto" />
@@ -39,7 +42,57 @@ export const ServerHeader = ({
             <DropdownMenuContent
               className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
             >
-
+                {isModerator && (
+                    <DropdownMenuItem
+                      className="text-indigo-500 px-3 py-2 text-sm cursor-pointer"
+                    >
+                        Invite People
+                        <UserPlus className="h-4 w-4 ml-auto"/>
+                    </DropdownMenuItem>
+                )}
+                {isAdmin && (
+                    <DropdownMenuItem
+                      className="text-zinc-400 px-3 py-2 text-sm cursor-pointer"
+                    >
+                        Server Settings
+                    <Settings className="h-4 w-4 ml-auto"/>
+                    </DropdownMenuItem>
+                )}
+                {isModerator && (
+                    <DropdownMenuItem
+                      className="text-zinc-400 px-3 py-2 text-sm cursor-pointer"
+                    >
+                        Manage Members
+                    <Users className="h-4 w-4 ml-auto"/>
+                    </DropdownMenuItem>
+                )}
+                {isModerator && (
+                    <DropdownMenuItem
+                      className="text-zinc-400 px-3 py-2 text-sm cursor-pointer"
+                    >
+                        Create Channel
+                    <PlusCircle className="h-4 w-4 ml-auto"/>
+                    </DropdownMenuItem>
+                )}
+                {isModerator && (
+                    <DropdownMenuSeparator />
+                )}
+                {isModerator && (
+                    <DropdownMenuItem
+                      className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                    >
+                        Delete Server
+                        <Trash className="h-4 w-4 ml-auto"/>
+                    </DropdownMenuItem>
+                )}
+                {!isAdmin && (
+                    <DropdownMenuItem 
+                      className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+                    >
+                        Leave Server
+                        <LogOut className="h-4 w-4 ml-auto" />
+                    </DropdownMenuItem>
+                )}
             </DropdownMenuContent>
         </DropdownMenu>
     )
